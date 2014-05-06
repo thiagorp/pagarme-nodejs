ee5974486a7b318a1f40d43155fcc0e04841a671
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2014-05-06 09:17:39 -0300

    Start api code

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-var PagarMe = {};
+var models = {
 
+};
 
+function PagarMe (key) {
 
-module.exports = PagarMe;
+    if (!(this instanceof PagarMe)) {
+        return new PagarMe(key);
+    }
+
+    this._api = {
+        key: key,
+        endpoint: 'https://api.pagar.me',
+        version: '1'
+    };
+
+    this._loadModels();
+};
+
+PagarMe.prototype = {
+
+    _loadModels: function() {
+        for (var model in models) {
+            this[model] = new models[model](this);
+        }
+    }
+
+};
+
+//module.exports = PagarMe;
